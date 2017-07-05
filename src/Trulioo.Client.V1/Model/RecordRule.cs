55c5e0eadb60d9d0f5822d6dc91e4cb6e9d6081f
Jeff <jeff@trulioo.com>
2017-07-05 14:52:21 -0700

    Remove redundant directive

--- src/Trulioo.Client.V1/Model/RecordRule.cs
+++ src/Trulioo.Client.V1/Model/RecordRule.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Trulioo.Client.V1.Model
+﻿namespace Trulioo.Client.V1.Model
 {
     public class RecordRule
     {
